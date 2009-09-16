c2c02797526bedf163113476ae6f24c536470fe3
Andrew Zotov <andrew.zotov@gmail.com>
2009-09-16 14:58:52 +0400

    restore normal indexing

--- app/models/activities/comment_subscription_activity.rb
+++ app/models/activities/comment_subscription_activity.rb
@@ -5,14 +5,14 @@ class CommentSubscriptionActivity < Activity
   # watchers are people who are subscribed to this entry comments
   has_many :watchers, :foreign_key => 'entry_id', :primary_key => 'entry_id', :class_name => 'FakeEntrySubscriber'
   
-  define_index do
-    indexes :id
-
-    has :user_id
-    has :entry_id
-    has :created_at
-    has watchers(:user_id), :as => :watcher_ids
-    
-    where "comments.id > #{Comment.last.id - 1000} AND is_disabled = 0"
-  end
+  # define_index do
+  #   indexes :id
+  # 
+  #   has :user_id
+  #   has :entry_id
+  #   has :created_at
+  #   has watchers(:user_id), :as => :watcher_ids
+  #   
+  #   where "comments.id > #{Comment.last.id - 1000} AND is_disabled = 0"
+  # end
 end
