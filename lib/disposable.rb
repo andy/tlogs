2c36afdfeb05b94965b302dca1ce07549c1e6cac
Andrew Zotov <andy@andy.local>
2010-06-13 19:46:28 +0400

    fixed a few bugs

--- lib/disposable.rb
+++ lib/disposable.rb
@@ -9,15 +9,19 @@ require 'json'
 
 module Disposable
   def is_disposable_email?(email)
-    req = Net::HTTP::Get.new("/services/json/isDisposableEmail/?email=#{email}")
-    res = Net::HTTP.start('www.undisposable.net') { |http| http.request(req) }
+    begin
+      req = Net::HTTP::Get.new("/services/json/isDisposableEmail/?email=#{email}")
+      res = Net::HTTP.start('www.undisposable.net') { |http| http.request(req) }
 
-    if res.is_a?(Net::HTTPOK)
-      json = JSON.parse(res.body)
-      return true if json['stat'] == 'ok' && json['email']['isdisposable'] == true
+      if res.is_a?(Net::HTTPOK)
+        json = JSON.parse(res.body)
+        return true if json['stat'] == 'ok' && json['email']['isdisposable'] == true
+      end
+
+      false
+    rescue
+      false
     end
-
-    false
   end
 
   module_function :is_disposable_email?
