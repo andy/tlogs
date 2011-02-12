9034e47e1e8f12a35c20a9d24a1a4047c8acbd60
Andrew Zotov <andy@andy.local>
2011-02-12 21:32:56 +0300

    log more data to analytics

--- public/javascripts/google-analytics.js
+++ public/javascripts/google-analytics.js
@@ -3,5 +3,7 @@ function ga_event(category, action, label, value) {
 	if(typeof _gaq != 'undefined') {
 		_gaq.push(['_trackEvent', category, action, label, value]);
 	}
+	
+	// console.log('goog track event, c=' + category + ' ' + ' a=' + action + ' l=' + label + ' v=' + value);
 	return true;
 }
