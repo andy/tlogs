71b88116c32186036a15a79269fc3c2b1be6b4ca
Andrew Zotov <andy@andy.local>
2011-02-20 21:17:42 +0300

    added redis configuration to config/redis.yml

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,10 @@
 require 'redis'
 
-$redis = Redis.new(:thread_safe => true)
+begin
+  redis_opts = YAML.load_file(File.join(RAILS_ROOT, 'config/redis.yml')).symbolize_keys!
+rescue
+  puts 'Redis configuration file (config/redis.yml) is missing'
+  exit
+end
+
+$redis = Redis.new(redis_opts)
