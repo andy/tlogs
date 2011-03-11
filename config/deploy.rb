cbdffc283fcde74c2ae3d74ce3e7aa230cbca4a5
Andrew Zotov <andy@andy.local>
2011-03-11 08:48:45 +0300

    this is not working

--- config/deploy.rb
+++ config/deploy.rb
@@ -191,8 +191,8 @@ namespace :deploy do
   end
 end
 
-Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
-  $: << File.join(vendored_notifier, 'lib')
-end
-
-require 'hoptoad_notifier/capistrano'
+# Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
+#   $: << File.join(vendored_notifier, 'lib')
+# end
+# 
+# require 'hoptoad_notifier/capistrano'
