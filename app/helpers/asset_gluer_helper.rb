073a2a03eb62c937e7857cba71e0be624ee831a5
Andrew Zotov <andrew.zotov@gmail.com>
2011-07-04 13:25:50 +0400

    ignore assets reboot issue

--- app/helpers/asset_gluer_helper.rb
+++ app/helpers/asset_gluer_helper.rb
@@ -3,13 +3,13 @@ module AssetGluerHelper
     if group = GROUPPED_STYLESHEETS[group_name]
       subgroup_suffix = subgroup == "default" ? "" : ".#{subgroup}"
       cached_file_name = "cache/#{group_name}#{subgroup_suffix}"
-      stylesheet_link_tag(group[subgroup], :cache => cached_file_name )
+      stylesheet_link_tag(group[subgroup], :cache => cached_file_name ) rescue nil
     end
   end
   def glued_javascript_include_tag(group_name)
     if group = GROUPPED_JAVASCRIPTS[group_name]
       cached_file_name = "cache/#{group_name}"
-      javascript_include_tag(group, :cache => cached_file_name )
+      javascript_include_tag(group, :cache => cached_file_name ) rescue nil
     end
   end
 end
