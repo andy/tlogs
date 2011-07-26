75ddd109f89346d0d644364270da0af2479dd31b
Andrew Zotov <andrew.zotov@gmail.com>
2011-07-26 15:48:22 +0400

    clean mini_magick uploads too

--- config/crontabs/app.rb
+++ config/crontabs/app.rb
@@ -5,6 +5,7 @@ job_type :bundle, "cd :path && RAILS_ENV=:environment bin/bundle exec :task :out
 # Remove rack uploads trash
 every 1.day do
   command "find /tmp/ -maxdepth 1 -type f -name RackMultipart\\* -ctime +1 -print0 -user tasty | xargs -0 rm"
+  command "find /tmp/ -maxdepth 1 -type f -name mini_magick\\* -ctime +1 -print0 -user tasty | xargs -0 rm"
 end
 
 every :reboot do
