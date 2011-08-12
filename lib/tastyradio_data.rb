f481e1d5b489c2fba7c6eaad840b792e5acbe3ec
Andrew Zotov <andrew.zotov@gmail.com>
2011-08-12 17:08:43 +0400

    Fixed current track displaying

--- lib/tastyradio_data.rb
+++ lib/tastyradio_data.rb
@@ -7,30 +7,39 @@ require 'uri'
 module TastyradioData
   def fetch
     begin
+      require 'hpricot'
+
       req = Net::HTTP::Get.new("/status2.xsl")
       res = Net::HTTP.start('radio.tlogs.ru', 8000) { |http| http.request(req) }
 
       if res.is_a?(Net::HTTPOK)
-        # xsl = Iconv.iconv('utf-8', 'windows-1251', res.body).to_s
-        xsl = res.body.to_s
+        doc = Hpricot.XML(res.body.to_s)
+        status = (doc/"//pre").first.html.split("\n").last.split(',')
 
         result = {
           :online     => 0,
           :song       => 'Не удалось получить данные'
         }
-        
-        result[:online] = xsl.split(',')[14] if xsl && xsl.split(',')[14]
-        result[:song]   = xsl.split(',')[16].strip if xsl && xsl.split(',')[16]
-        
-        result[:song] = 'неизвестный трек' if result[:song] && result[:song].length < 3
 
+        offset = (status.length / 6) - 1
+        if offset >= 0
+          pp offset
+          data = status[(offset*6)...((offset + 1) * 6)]
+          pp data
+          result[:online] = data[3]
+          result[:song]   = data[5].strip.gsub(/^\-\s+/, '').gsub(/\s+\-$/, '')
+        
+          result[:song] = 'неизвестный трек' if result[:song] && result[:song] == '-'
+        end
+
+        
         return result
       end
 
       nil
-    rescue
-
-      nil
+    # rescue
+    # 
+    #   nil
     end
     
   end
