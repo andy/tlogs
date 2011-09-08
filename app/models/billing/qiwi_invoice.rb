6fde85c41989e607c699a113a1aa7b68b35f7c71
Andrew Zotov <andrew.zotov@gmail.com>
2011-09-08 14:05:01 +0400

    annotated model

--- app/models/billing/qiwi_invoice.rb
+++ app/models/billing/qiwi_invoice.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+# Schema version: 20110816190509
+#
+# Table name: invoices
+#
+#  id         :integer(4)      not null, primary key
+#  user_id    :integer(4)      not null, indexed
+#  state      :string(255)     not null
+#  type       :string(255)     not null
+#  metadata   :string(255)     not null
+#  amount     :float           default(0.0), not null
+#  revenue    :float           default(0.0), not null
+#  days       :integer(4)      default(0), not null
+#  remote_ip  :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+# Indexes
+#
+#  index_invoices_on_user_id  (user_id)
+#
+
 #
 # Qiwi through SOAP API, as documented in https://ishop.qiwi.ru/docs/OnlineStoresProtocols_SOAP.pdf
 #
