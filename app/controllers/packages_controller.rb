b11e66d13f900a56340ba06ee964dd2f17708700
Andrew Zotov <andrew.zotov@gmail.com>
2011-09-17 14:02:01 +0400

    make this extremely efficient

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,15 +1,24 @@
 class PackagesController < ApplicationController
+  skip_before_filter :preload_is_private
+  skip_before_filter :preload_current_service
+  skip_before_filter :preload_current_site
+  skip_before_filter :preload_current_user
+
   
   def css
-    files   = TASTY_ASSETS[:stylesheets][params[:name]]
-    result  = AssetGluer::StylesheetFile.glue(files)
+    files     = TASTY_ASSETS[:stylesheets][params[:name]]
+    key       = files.each { |f| Digest::SHA1.file(f).hexdigest }.join(':')
+    cache_key = Digest::SHA1.hexdigest(key)
+    result    = Rails.cache.fetch(cache_key, :expires_in => 1.day) { AssetGluer::StylesheetFile.glue(files) }
 
     render :text => result, :content_type => 'text/css'
   end
   
   def js
-    files   = TASTY_ASSETS[:javascripts][params[:name]]
-    result  = AssetGluer::JavascriptFile.glue(files)
+    files     = TASTY_ASSETS[:javascripts][params[:name]]
+    key       = files.each { |f| Digest::SHA1.file(f).hexdigest }.join(':')
+    cache_key = Digest::SHA1.hexdigest(key)
+    result    = Rails.cache.fetch(cache_key, :expires_in => 1.day) { AssetGluer::JavascriptFile.glue(files) }
 
     render :text => result, :content_type => 'text/javascript'
   end
