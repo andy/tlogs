66d53d18d231185a596e666bc1d1088bcaceace4
Andrew Zotov <andrew.zotov@gmail.com>
2011-11-25 19:38:37 +0400

    interesting people

--- app/models/user.rb
+++ app/models/user.rb
@@ -144,6 +144,24 @@ class User < ActiveRecord::Base
 
 
   ## class methods
+  def self.active_for(user, options = {})
+    options.reverse_merge!(:limit => 6, :popularity => 2)
+    
+    user_ids = Rails.cache.fetch("users::active", :expires_in => 10.minutes) do
+      Relationship.find(:all, :select => 'user_id', :order => 'id desc', :limit => 1000).map(&:user_id)
+    end
+    
+    user_ids =  user_ids.sort.                        # sort numerically
+                group_by { |x| x }.                   # group the same accounts together
+                sort_by { |k, v| v.length }.          # sort by popularity
+                select { |k, v| v.length >= options[:popularity] }.  # but select only people who have at least more active subscribers
+                map(&:first).                         # keep only ids
+                reverse
+
+    User.find_all_by_id(user_ids.shuffle[0...(options[:limit] * 2)], :include => [:avatar, :tlog_settings]).select { |u| u.can_be_viewed_by?(user) }[0..options[:limit]]
+  end
+
+  
   ## public methods
 
   # Пример: <%= user.gender("он", "она") %>
