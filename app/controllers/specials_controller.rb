83e5a8e22a0cad5ed07fb041b9c0be0322261276
Andrew Zotov <andrew.zotov@gmail.com>
2011-12-31 05:03:43 +0400

    God fucking damn, chats must be much more easiter to do in '11

--- app/controllers/specials_controller.rb
+++ app/controllers/specials_controller.rb
@@ -1,8 +1,11 @@
 class SpecialsController < ApplicationController
   
+  def index
+    redirect_to '/'
+  end
 
   def surfingbird
-    @title = "Surfingbird — мы подбираем интересные страницы, фото и видео на основе рекомендаций людей, которым нравится то же, что и тебе."
+    @title = "Surfingbird — мы подбираем интересные страницы, фото и видео на основе рекомендаций людей, которым нравится то же, что и тебе."
 
     sb_config = YAML.load_file(File.join(Rails.root, 'config/specials/surfingbird.yml')).symbolize_keys!
     
@@ -20,10 +23,71 @@ class SpecialsController < ApplicationController
     render :layout => false
   end
   
-  def foreveralonenewyear
+  def newyear
     @title = "Forever Alone Tasty New Year"
     
     @addr  = Ipgeobase.lookup(request.remote_ip)
     @city  = @addr[:city] if @addr && @addr[:city]
+    
+    @autojoin = ['Прямой эфир', @city].compact
   end
+  
+  def join
+    chat = TastyChat.for(:user => current_user, :redis => $redis)
+    chan = chat.channel params[:name]
+    chan.subscribe!
+
+    render :json => [make_channel_recent(chan)]
+  end
+  
+  def leave
+    chat = TastyChat.for(:user => current_user, :redis => $redis)
+    chan = chat.channel params[:name]
+    chan.unsubscribe!
+    
+    render :json => { :name => chan.name, :uuid => chan.uuid }
+  end
+  
+  def post
+    chat = TastyChat.for(:user => current_user, :redis => $redis)
+    chan = chat.channel params[:name]
+    
+    chan.post params[:text]
+    
+    render :json => true
+  end
+  
+  def read
+    chat = TastyChat.for(:user => current_user, :redis => $redis)
+
+    result = []
+    chat.channels.each { |chan| result << make_channel_recent(chan) }
+    
+    render :json => result
+  end
+  
+  protected
+    def make_channel_recent(channel)
+      recent = channel.recent.sort_by { |message| message[:id] }.map do |message|
+        # cache user details
+        user_details = Rails.cache.fetch("chat:resolve:#{message[:user]}", :expires_in => 1.hour) do
+          user    = User.find(message[:user])
+          userpic = user.userpic? ? user.userpic.url : (user.avatar ? user.avatar.public_filename : nil)
+
+          { :url => user.url, :userpic => userpic }
+        end
+        
+        # result
+        {
+          :id       => [channel.uuid, message[:id]].join('-'),
+          :text     => message[:text],
+          :iso8601  => Time.at(message[:stamp]).getutc.iso8601,
+          :time     => Russian::strftime(Time.at(message[:stamp]), '%d %B %Y в %H:%M'),
+          :url      => user_details[:url],
+          :userpic  => user_details[:userpic]
+        }
+      end
+      
+      { :name => channel.name, :uuid => channel.uuid, :messages => recent }
+    end
 end
---
+++ lib/tasty_chat.rb
@@ -0,0 +1,98 @@
+class TastyChat
+  attr_accessor :user, :redis
+
+  def initialize options
+    options.assert_valid_keys(:user, :redis)
+
+    @user   = options[:user]
+    @redis  = options[:redis]
+  end
+  
+  def channel name
+    TastyChatChannel.new self, name
+  end
+
+  #
+  # Subscriptions
+  #
+  
+  def recent
+    channels.map(&:recent)
+  end
+
+  def channels
+    @redis.smembers(subs_key).map { |name| channel(name) }
+  end
+  
+  def subs_key
+    ['chat', 'subs', @user.id].join(':')
+  end
+
+  def counter_key
+    ['chat', 'counters'].join(':')
+  end
+  
+  #
+  # Class methods
+  #
+  def self.for options
+    self.new options
+  end
+end
+
+class TastyChatChannel
+  attr_accessor :name
+
+  def initialize chat, name
+    @chat = chat
+    @name = name
+  end
+  
+  def uuid
+    Digest::SHA1.hexdigest(@name)[0..8]
+  end
+
+
+  def subscribe!
+    @chat.redis.sadd @chat.subs_key, @name
+  end
+  
+  def unsubscribe!
+    @chat.redis.srem @chat.subs_key, @name
+  end
+  
+  def subscribed?
+    @chat.redis.sismember @chat.subs_key, @name
+  end
+  
+  def post text
+    subscribe! unless subscribed?
+
+    message     = { :id      => gen_message_id!,
+                    :stamp   => Time.now.to_i,
+                    :text    => text,
+                    :user    => @chat.user.id
+                  }
+
+    count = @chat.redis.lpush channel_key, Base64.encode64(Marshal.dump(message))
+    
+    # truncage channel if it gets too long
+    @chat.redis.ltrim(channel_key, 0, 1000) if count > 1000
+  end
+  
+  def recent(count = 20)
+    @chat.redis.lrange(channel_key, 0, count).map { |message| Marshal.load(Base64.decode64(message)) }
+  end
+  
+  def channel_key
+    ['chat', 'chan', @name].join(':')
+  end
+
+  def gen_message_id!
+    @chat.redis.hincrby @chat.counter_key, @name, 1
+  end
+  
+  def cur_message_id
+    @chat.redis.hget @chat.counter_key, @name
+  end
+end
