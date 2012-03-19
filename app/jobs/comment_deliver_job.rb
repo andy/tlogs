88093c35ac9cb9ac3b1cbbaaea9b883b815521c9
Andrew Zotov <andrew.zotov@gmail.com>
2012-03-19 12:39:22 +0400

    deliver only valid comments, drop other

--- app/jobs/comment_deliver_job.rb
+++ app/jobs/comment_deliver_job.rb
@@ -4,7 +4,7 @@ class CommentDeliverJob
   def self.perform(comment_id, service_domain, reply_to)
     comment = Comment.find_by_id(comment_id)
     
-    if comment
+    if comment && comment.entry && !comment.entry.is_disabled? && comment.user && !comment.user.is_disabled?
       current_service = Tlogs::Domains::CONFIGURATION.options_for(service_domain, nil)
       comment.deliver!(current_service, reply_to)
     end
