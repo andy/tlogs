bfdeca33730493fd0a01d157955baea8c65da8e6
Andrew Zotov <andrew.zotov@gmail.com>
2012-03-30 12:30:20 +0400

    tiny updates

--- script/weblogs_ping.rb
+++ script/weblogs_ping.rb
@@ -4,6 +4,50 @@ require 'xmlrpc/client'
 
 trap(:INT) { puts; exit }
 
+counter = {}
+
+def efetch(entry_id)
+  begin
+    entry = Entry.find(entry_id, :include => { :author => [ :tlog_settings ]})      
+  rescue ActiveRecord::RecordNotFound
+    return nil
+  end
+  user  = entry.author
+    
+  if entry.is_private?
+    puts "- skipping private entry (#{user.url}, #{entry.id})"
+    return false
+  end
+
+  if user.tlog_settings.privacy != 'open'
+    puts "- skipping private tlog (#{user.url}, #{entry.id})"
+    return false
+  end
+
+  entry
+end
+
+def yaping(user, entry)
+  desc = user.url
+  desc += ' — ' + user.tlog_settings.about unless user.tlog_settings.about.blank?
+
+  server = XMLRPC::Client.new2('http://ping.blogs.yandex.ru/RPC2')
+  begin
+    result = server.call('weblogUpdates.ping', desc, "http://#{user.url}.mmm-tasty.ru/feed/rss.xml")
+  rescue Exception => ex
+    puts "- ping failed: #{ex.class.name}: #{ex.message}"
+    next
+  end
+
+  if result && result['flerror']
+    puts "- ping failed: #{result['message']}"
+  elsif result
+    puts "+ ping (#{user.url}, #{entry.id})"
+  else
+    puts "- ping failed: unknown reason"
+  end
+end
+
 $redis.subscribe(:ping) do |on|
   on.subscribe do |channel, subs|
     puts "+ subscribed to channel ##{channel}"
@@ -12,46 +56,38 @@ $redis.subscribe(:ping) do |on|
   on.message do |channel, message|
     rt = 0
 
-    begin
-      entry = Entry.find(message, :include => { :author => [ :tlog_settings ]})      
-    rescue ActiveRecord::RecordNotFound
-      if rt > 1
-        puts "- failed to find (#{message})"
-        next
+    entries = []
+
+    # Load entries to retry ping with
+    counter.keys.each do |entry_id|
+      if counter[entry_id] > 30
+        puts "- entry not found (#{entry_id})"
+	counter.delete entry_id
+      else
+	entry = efetch(entry_id)
+	if entry.nil?
+	  counter[entry_id] += 1
+	elsif entry
+	  counter.delete entry_id
+	  entries << entry
+	else
+	  counter.delete entry_id
+	end
       end
-      rt += 1
-      sleep(1)
-      retry 
-    end
-    user  = entry.author
-    
-    if entry.is_private?
-      puts "- skipping private entry (#{user.url}, #{entry.id})"
-      next 
     end
 
-    if user.tlog_settings.privacy != 'open'
-      puts "- skipping private tlog (#{user.url}, #{entry.id})"
-      next
-    end
-    
-    desc = user.url
-    desc += ' — ' + user.tlog_settings.about unless user.tlog_settings.about.blank?
-
-    server = XMLRPC::Client.new2('http://ping.blogs.yandex.ru/RPC2')
-    begin
-      result = server.call('weblogUpdates.ping', desc, "http://#{user.url}.mmm-tasty.ru/feed/rss.xml")
-    rescue Exception => ex
-      puts "- ping failed: #{ex.class.name}: #{ex.message}"
-      next
+    # Load current entry
+    entry = efetch(message)
+    if entry.nil?
+      counter[message] = 1
+    elsif entry
+      entries << entry
     end
 
-    if result && result['flerror']
-      puts "- ping failed: #{result['message']}"
-    elsif result
-      puts "+ ping (#{user.url}, #{entry.id})"
-    else
-      puts "- ping failed: unknown reason"
+    # Now ping all entries we've collected
+    entries.each do |entry|
+      yaping(entry.author, entry)
+      counter.delete entry.id
     end
   end
 end
