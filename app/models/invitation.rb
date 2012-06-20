8c7d080d98966e6c5acacabea55af32faabf45a8
Andrew Zotov <andrew.zotov@gmail.com>
2012-06-20 20:32:45 +0400

    typo

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,6 +1,6 @@
 class Invitation < ActiveRecord::Base
   belongs_to :user
-  belongs_to :invitee, :class_name => User
+  belongs_to :invitee, :class_name => 'User'
   
   validates_presence_of       :email,
                               :on => :create,
