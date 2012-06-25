0d027e77cd5fd00aeac2185d9b6244720201b8a1
Andrew Zotov <andrew.zotov@gmail.com>
2012-06-26 01:43:53 +0400

    ignore propfind exception

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,8 @@
 HoptoadNotifier.configure do |config|
   config.api_key = '80f689a37ac40bbf38e841ad2d93a255'
+  
+  config.ignore_by_filter do |exception_data|
+    exception_data.error_message =~ "ActionController::UnknownHttpMethod: PROPFIND"
+  end
 end
+
