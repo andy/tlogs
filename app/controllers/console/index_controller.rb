d3a0c1de951b770a5f7bacbb327580a70a6a6489
Andrew Zotov <andrew.zotov@gmail.com>
2012-07-12 02:11:37 +0400

    make blacklist work only for premium users

--- app/controllers/console/index_controller.rb
+++ app/controllers/console/index_controller.rb
@@ -7,7 +7,7 @@ class Console::IndexController < ConsoleController
     query = params[:query]
     
     user   = User.find_by_id(query) if query =~ /^\d+$/
-    user ||= User.find_by_email(query) if query =~ /\@/
+    user ||= User.find_by_email(query) if query =~ /@/
     user ||= User.find_by_url(query)
     
     render :action => 'not_found', :status => 404 and return unless user
