42e2732be41fb9ff2eae19f3445ccfcb062a75d1
Andrew Zotov <andrew.zotov@gmail.com>
2012-08-05 20:46:04 +0400

    Feature: improve console lookup

--- app/controllers/console/index_controller.rb
+++ app/controllers/console/index_controller.rb
@@ -4,7 +4,13 @@ class Console::IndexController < ConsoleController
   end
   
   def lookup
-    query = params[:query]
+    query = params[:query] || ''
+
+    # match ip address
+    redirect_to ip_console_changelogs_path(:addr => query) and return if query =~ /\A\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\Z/i
+    
+    # match url (extract first part of hostname)
+    query = URI.parse(query).host.split(/\./)[0] if query.starts_with?('http://')    
     
     user   = User.find_by_id(query) if query =~ /^\d+$/
     user ||= User.find_by_email(query) if query =~ /@/
